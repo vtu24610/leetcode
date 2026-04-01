951e055689f1e831eaaa5b909ecb831f312548cf
vtu24610 <vtu24610@veltech.edu.in>
2026-04-01 10:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (99.35%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,25 +1,15 @@
-import java.util.*;
-
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(p);
-        queue.add(q);
-        
-        while (!queue.isEmpty()) {
-            TreeNode node1 = queue.poll();
-            TreeNode node2 = queue.poll();
-            
-            if (node1 == null && node2 == null) continue;
-            if (node1 == null || node2 == null) return false;
-            if (node1.val != node2.val) return false;
-            
-            queue.add(node1.left);
-            queue.add(node2.left);
-            queue.add(node1.right);
-            queue.add(node2.right);
-        }
-        
-        return true;
+        // both null
+        if (p == null && q == null) return true;
+
+        // one null
+        if (p == null || q == null) return false;
+
+        // values differ
+        if (p.val != q.val) return false;
+
+        // check left and right subtrees
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
