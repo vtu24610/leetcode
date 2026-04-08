e520b9ae5319fe67815cb6569a4301020efd23e8
vtu24610 <vtu24610@veltech.edu.in>
2026-04-08 09:31:11 +0530

    Time: 11 ms (96.14%) | Memory: 47.4 MB (84.89%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        // Step 1: Frequency map
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Step 2: Buckets
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+        for (int key : freqMap.keySet()) {
+            int freq = freqMap.get(key);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(key);
+        }
+
+        // Step 3: Collect top k
+        int[] result = new int[k];
+        int index = 0;
+
+        for (int i = buckets.length - 1; i >= 0 && index < k; i--) {
+            if (buckets[i] != null) {
+                for (int num : buckets[i]) {
+                    result[index++] = num;
+                    if (index == k) break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
